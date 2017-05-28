8aba7a46a152ea9a4d02c095ccb349e64d9f5e63
Marc Ballarin <ballarin.marc@gmx.de>
2017-05-28 16:58:42 +0200

    run through perltidy

--- ksysguard-nvidia-smi/ksysguard-nvidia-smi.pl
+++ ksysguard-nvidia-smi/ksysguard-nvidia-smi.pl
@@ -12,29 +12,37 @@ use List::Util qw(reduce);
 
 =head1 DESCRIPTION
 
-A script that implements the KSysGuard protocol described at https://techbase.kde.org/Development/Tutorials/Sensors
-and interfaces with Nvidia's nvidia-smi tool.
+A script that implements the KSysGuard protocol described at 
+https://techbase.kde.org/Development/Tutorials/Sensors and interfaces with 
+Nvidia's nvidia-smi tool
+(https://developer.nvidia.com/nvidia-system-management-interface).
 
-It should support an arbitrary number of GPUs and provides the following values:
-fan speed (%), temperature (°C), performance level, power usage (W), power_cap (W), 
-memory usage (MiB), memory total (MiB), GPU utilization (%).
+It should support an arbitrary number of GPUs and provides the following 
+values:
+fan speed (%), temperature (°C), performance level, power usage (W),
+power_cap (W), memory usage (MiB), memory total (MiB), GPU utilization (%).
 
-By default, this script will fetch new data at most every two seconds. It is possible, this needs to be increased,
-if many GPUs are installed.
+By default, this script will fetch new data at most every two seconds. It is 
+possible, this needs to be increased, if many GPUs are installed.
 
 =head1 USAGE
 
 Save this script somewhere - let's assume /usr/local/bin.
 
-In KSysGuard, go to File->Monitor Remote Machine. Enter nvidia-smi as host, then select "Custom command" as "Connection Type"
-and enter "perl /usr/local/bin/ksysguard-nvidia-smi.pl" (without the quotation marks) as command. Replace /usr/local/bin with
-wherever you placed the script. Note that KSysGuard does not expand ~ to your home folder.
+In KSysGuard, go to File->Monitor Remote Machine. Enter nvidia-smi as host, then 
+select "Custom command" as "Connection Type" and enter 
+"perl /usr/local/bin/ksysguard-nvidia-smi.pl" (without the quotation marks) as
+command. Replace /usr/local/bin with wherever you placed the script. 
+
+Note that KSysGuard does not expand ~ to your home folder.
 
 =head1 ISSUES
 
-This script really doesn't care about error handling. I don't even know, if the KSysGuard protocol supports error reporting.
-If it does not work, start by running the command indicated by $NVIDIA_SMI below manually and check if this outputs at least
-one line like the following:
+This script really doesn't care about error handling. I don't even know, If
+the KSysGuard protocol supports error reporting.
+
+If it does not work, start by running the command indicated by $NVIDIA_SMI 
+below manually and check if this outputs at least one line like the following:
 
     |  0%   49C    P8    14W / 240W |    309MiB /  8110MiB |      0%      Default |
 
@@ -42,11 +50,23 @@ one line like the following:
 
 Copyright 2017 Marc Ballarin
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation  the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 =cut
 
@@ -56,19 +76,67 @@ my $CACHE_SECONDS = 2;
 # final field name will be 'name' with GPU ID appended, i.e. fan_speed0, fan_speed1, ...
 # 'max' is a coderef that will be called with a hashref of per-GPU data as argument
 my @FIELD_SPEC = (
-    { name => 'fan_speed',  desc => 'Fan speed',         unit => '%',   re => qr/.+?(\d+)%/x,      max => sub { 100 } },
-    { name => 'temp',       desc => 'GPU temperature',   unit => '°C',  re => qr/\s+(\d+)C/x,      max => sub { 100 } },
-    { name => 'perf_level', desc => 'Performance level', unit => '',    re => qr/\s+P(\d+)/x,      max => sub { 10 } },
-    { name => 'pwr_usage',  desc => 'Power usage',       unit => 'W',   re => qr/\s+(\d+)W/x,      max => sub { $_[0]->{pwr_cap} } },
-    { name => 'pwr_cap',    desc => 'Power cap',         unit => 'W',   re => qr/\s+\/\s+(\d+)W/x, max => sub { $_[0]->{pwr_cap} } },
-    { name => 'mem_usage',  desc => 'Memory usage',      unit => 'MiB', re => qr/.+?(\d+)MiB/x,    max => sub { $_[0]->{mem_total} } },
-    { name => 'mem_total',  desc => 'Memory total',      unit => 'MiB', re => qr/.+?(\d+)MiB/x,    max => sub { $_[0]->{mem_total} } },
-    { name => 'util',       desc => 'Utilization',       unit => '%',   re => qr/.+?(\d+)%/x,      max => sub { 100 } },
+    {
+        name => 'fan_speed',
+        desc => 'Fan speed',
+        unit => '%',
+        re   => qr/.+?(\d+)%/x,
+        max  => sub { 100 }
+    },
+    {
+        name => 'temp',
+        desc => 'GPU temperature',
+        unit => '°C',
+        re   => qr/\s+(\d+)C/x,
+        max  => sub { 100 }
+    },
+    {
+        name => 'perf_level',
+        desc => 'Performance level',
+        unit => '',
+        re   => qr/\s+P(\d+)/x,
+        max  => sub { 10 }
+    },
+    {
+        name => 'pwr_usage',
+        desc => 'Power usage',
+        unit => 'W',
+        re   => qr/\s+(\d+)W/x,
+        max  => sub { $_[0]->{pwr_cap} }
+    },
+    {
+        name => 'pwr_cap',
+        desc => 'Power cap',
+        unit => 'W',
+        re   => qr/\s+\/\s+(\d+)W/x,
+        max  => sub { $_[0]->{pwr_cap} }
+    },
+    {
+        name => 'mem_usage',
+        desc => 'Memory usage',
+        unit => 'MiB',
+        re   => qr/.+?(\d+)MiB/x,
+        max  => sub { $_[0]->{mem_total} }
+    },
+    {
+        name => 'mem_total',
+        desc => 'Memory total',
+        unit => 'MiB',
+        re   => qr/.+?(\d+)MiB/x,
+        max  => sub { $_[0]->{mem_total} }
+    },
+    {
+        name => 'util',
+        desc => 'Utilization',
+        unit => '%',
+        re   => qr/.+?(\d+)%/x,
+        max  => sub { 100 }
+    },
 );
 
 my %fields_by_name = map { ($_->{name}, $_) } @FIELD_SPEC;
 my $re_fields = reduce { qr/$a $b/x } map { $_->{re} } @FIELD_SPEC;
-my $re_input  = qr/^(\w+?)(\d+)(\?)?$/x;
+my $re_input = qr/^(\w+?)(\d+)(\?)?$/x;
 
 sub print_monitors {
     my ($data) = @_;
@@ -88,8 +156,7 @@ sub print_field_on_gpu {
 
     if (my $gpu_data = $data->[$gpu_id]) {
         if ($do_meta) {
-            printf(
-                "%s\t0\t%d\t%s\n",
+            printf("%s\t0\t%d\t%s\n",
                 $fields_by_name{$field_name}->{desc},
                 get_max($field_name, $gpu_data),
                 $fields_by_name{$field_name}->{unit},
@@ -111,6 +178,7 @@ sub get_max {
 {
     my $cached_data;
     my $last_checked = 0;
+
     sub collect_data {
         if ($last_checked < time() - $CACHE_SECONDS) {
             my @gpu_data;
